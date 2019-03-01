c6a8bcdcbc8fbdf600912e0cdd272886c89dce66
daanielowsky <pawlicki.daniel@wp.pl>
2019-03-01 22:36:02 +0100

    Dodano brak powtarzalności liczb

--- src/pl/dpawlicki/workshops_1/lotto/LottoApp.java
+++ src/pl/dpawlicki/workshops_1/lotto/LottoApp.java
@@ -19,23 +19,39 @@ public class LottoApp {
         int[] yourNumbers = new int[6];
         int number = 0;
 
-
         lettering("Dzisiaj zagramy w Lotto!");
         System.out.println("Sprawdźmy czy masz dzisiaj szczęście.");
         System.out.println("Podaj swoje liczby do losowania. ");
+
         Scanner scan = new Scanner(System.in);
+
         int i = 1;
         int j = 0;
+
         while (j < 6) {
+
             try {
+
                 System.out.print("Podaj swoją " + i + " liczbę: ");
                 number = scan.nextInt();
+
                 if (number > 0 && number <= 49) {
+
+                    for (int u = 0; u < yourNumbers.length; u++) {
+
+                        if (yourNumbers[u] == number) {
+                            System.out.println("Użyłeś już takiej liczby!");
+                            System.out.print("Podaj swoją " + i + " liczbę: ");
+                            number = scan.nextInt();
+                        }
+                    }
                     yourNumbers[j] = number;
                     System.out.println(Arrays.toString(yourNumbers));
                     i++;
                     j++;
+
                 } else {
+
                     System.out.println("W lotto liczby są w przedziale od 1 do 49!");
                 }
             } catch (InputMismatchException exp) {
@@ -50,9 +66,13 @@ public class LottoApp {
         System.out.println();
         lettering(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3] + " " + arr[4] + " " + arr[5]);
         System.out.println();
+
         int win = 0;
+
         for (int ch = 0; ch < yourNumbers.length; ch++) {
+
             for (int hc = 0; hc < yourNumbers.length; hc++) {
+
                 if (arr[ch] == yourNumbers[hc]) {
                     win++;
                 }
