f67ba8a843c920de6a3d57fa2053f10bbed103a7
daanielowsky <pawlicki.daniel@wp.pl>
2019-03-06 22:12:10 +0100

    Lekkie poprawki

--- src/pl/dpawlicki/workshops_1/cube/Dice.java
+++ src/pl/dpawlicki/workshops_1/cube/Dice.java
@@ -4,7 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
-public class cube {
+public class Dice {
 
     public static void main(String[] args) throws InterruptedException {
 
